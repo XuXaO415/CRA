e01ca7e235e97cefc65045c710f1d6f74eb25928
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-03-29 15:19:17 -0700

    Added svg

--- first-new-app/src/App.js
+++ first-new-app/src/App.js
@@ -3,13 +3,19 @@
 // import data, {meow} from './cats';
 import data from './random';
 import RandomData from './RandomData';
+import fakeLogo from './fakeLogo.svg';
+// import moreData from './moreData';
 import './App.css';
 
 
 function App() {
- 
+//  console.log(fakeLogo);
   return (
-  <RandomData data={data}/>
+    <div>
+    <img src={fakeLogo} width="150" />
+      <RandomData data={data} username="Amy" />
+      {/* <RandomData data={moreData} username="Diego" /> */}
+    </div>
   );
 }
 
